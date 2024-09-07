ffd8c125e0063a75df6b33a6925c55bb66f3d9d3
Sandile M'remi <sm-remi023@student.wethinkcode.co.za>
2024-09-07 11:16:28 +0200

    fixed cards

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -12,19 +12,19 @@ const Hero = () => {
                 <Image src={shield} alt="" className="inline-flex items-center justify-center px-5 py-3"/>
                     <div className="flex gap-8 mt-8">
 
-                    <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 bg-gray-200 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <a href="#" className="block max-w-sm p-6 rounded-lg shadow bg-gray-100 bg-gray-200">
 
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">Secure</h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">Keep your passwords safe with top-notch encryption.</p>
                     </a>
 
-                    <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 bg-gray-200 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <a href="#" className="block max-w-sm p-6 rounded-lg shadow bg-gray-100 bg-gray-200">
 
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Sync</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                     </a>
 
-                    <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 bg-gray-200 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <a href="#" className="block max-w-sm p-6 rounded-lg shadow bg-gray-100 bg-gray-200">
 
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Protect</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -37,6 +37,9 @@ const Hero = () => {
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
                 </a> */}
+                {/* <h1 className="mt-10 text-4xl font-extrabold leading-none tracking-tight text-black md:text-5xl lg:text-2xl">Get started with VaultLock</h1> */}
+
+
 
             </div>
        
