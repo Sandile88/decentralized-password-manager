bebd7f6e1baefe84bca59197038d86fc84902ad6
Sandile M'remi <sm-remi023@student.wethinkcode.co.za>
2024-10-02 15:21:16 +0200

    finished modal

--- client/components/Modal.tsx
+++ client/components/Modal.tsx
@@ -10,18 +10,18 @@ const Modal: React.FC<ModalProps> = ({isOpen, onClose}) => {
     if (!isOpen) return null; // Return nothing if the modal is not open
 
     return (
-        <div id="add-passwords" tabIndex={-1} aria-hidden={!isOpen} 
+        <div id=" add-passwords" tabIndex={-1} aria-hidden={!isOpen} 
              className={`${isOpen ? 'flex' : 'hidden'} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
             <div className="relative p-4 w-full max-w-md max-h-full">
-                <div className="relative rounded-lg shadow bg-white">
-                    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-blue-600">
+                <div className="relative rounded-3xl shadow-xl bg-white">
+                    <div className="flex items-center justify-between p-4 md:p-5 rounded-t ">
                         <h3 className="text-xl font-semibold  text-black">
-                            Add Password
+                            Save Password
                         </h3>
-                        <button type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        <button type="button" className="end-2.5 bg-transparent rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
                                 onClick={onClose}
                         >
-                            <svg className="w-3 h-3 text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <svg className="w-2 h-3 text-blue-600 hover:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                             </svg>
                             <span className="sr-only">Close modal</span>
@@ -30,29 +30,28 @@ const Modal: React.FC<ModalProps> = ({isOpen, onClose}) => {
                     <div className="p-4 md:p-5">
                         <form className="space-y-4" action="#">
                         <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">Name</label>
-                                <input type="email" name="email" id="email" className="bg-gray-200 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-blue-600 dark:placeholder-blue-400 " placeholder="name@company.com" required />
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">Website Name/URL</label>
+                                <input type="email" name="email" id="email" className="bg-gray-200 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:placeholder-blue-400 " placeholder="name@company.com" required />
                             </div>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">Username</label>
-                                <input type="email" name="email" id="email" className="bg-blue-600 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-blue-600 dark:placeholder-blue-400 " placeholder="name@company.com" required />
+                                <input type="email" name="email" id="email" className="bg-gray-200 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-blue-400 " placeholder="pluto67#!" required />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-black">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-blue-600 dark:placeholder-blue-400" required />
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-200 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:placeholder-blue-400" required />
                             </div>
                             <div className="flex justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                                <div className="flex mb-4 items-start">
+                                    
+                                    <div className="flex items-center">
+                                    <button type="button" className=" text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+                                    <button type="button" className="text-white bg-black hover:bg-slate-800 focus:outline-none focus:ring-4 focus:ring-black font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">Cancel</button>
                                     </div>
-                                    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
                                 </div>
-                                <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-                            </div>
-                            <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                            <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                                <div>
+                                <button type="button" className=" text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delete</button>
+                                </div>
                             </div>
                         </form>
                     </div>
