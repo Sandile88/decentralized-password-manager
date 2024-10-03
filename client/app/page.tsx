0423a9b3186aaa6aa8857c47a0f8df8c02a0ffd9
Sandile M'remi <sm-remi023@student.wethinkcode.co.za>
2024-10-03 19:36:17 +0200

    Updated page.tsx and added Services component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import Services from "../components/Services";
 
 const LandingPage = () => {
   return (
@@ -8,6 +9,7 @@ const LandingPage = () => {
     <section id="hero" >
       <Hero/>
     </section>
+    <Services/>
     <section>
       <Footer/>
     </section>
