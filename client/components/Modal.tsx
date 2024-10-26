3499e063744ff62bd9d292b1cf8b3878d8fb5698
Sandile M'remi <sm-remi023@student.wethinkcode.co.za>
2024-10-26 10:55:43 +0200

    password can be saved

--- client/components/Modal.tsx
+++ client/components/Modal.tsx
@@ -53,15 +53,15 @@ const Modal: React.FC<ModalProps> = ({isOpen, onClose}) => {
                         <form className="space-y-4" action="#">
                         <div>
                                 <label htmlFor="resource" className="block mb-2 text-sm font-medium text-gray-600">Website Name/URL</label>
-                                <input type="text" name="resource" id="resouce" className="bg-gray-200 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:placeholder-blue-400 " placeholder="name@company.com" required />
+                                <input type="text" name="resource" id="resouce" className="bg-gray-200 border border-gray-300 text-black text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:placeholder-blue-400 " placeholder="name@company.com" required />
                             </div>
                             <div>
                                 <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-600">Username</label>
-                                <input type="text" name="username" id="username" className="bg-gray-200 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-blue-400 " placeholder="pluto67#!" required />
+                                <input type="text" name="username" id="username" className="bg-gray-200 border border-gray-300 text-black text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-blue-400 " placeholder="pluto67#!" required />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-600">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-200 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:placeholder-blue-400" required />
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-200 border border-gray-300 text-black text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:placeholder-blue-400" required />
                             </div>
                             <div className="flex justify-between">
                                 <div className="flex mb-4 items-start">
