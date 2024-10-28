4171862f3ba14d250be7eb691f7edfa9e70302a5
Sandile M'remi <sm-remi023@student.wethinkcode.co.za>
2024-10-28 15:00:16 +0200

    fixed nav bar elements for different end points

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,120 +1,69 @@
 "use client"
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import logo from "../public/logo.svg";
-import { contractInstance }  from "../components/Wallet";
-// import { Link } from "react-scroll";
-import Link from "next/link";
-import Web3 from "web3";
-import homee from "../app/home/page";
-// import { useRouter } from "next/router";
-// import { useRouter } from "next/navigation";
-// import  Wallet  from "../components/Wallet";
+import React, { useEffect, useState } from "react";
 import { useWallet } from "../components/Wallet";
-import { Link as ScrollLink} from "react-scroll";
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
+import logo from "@/public/logo.svg";
+import Image from "next/image";
 
 const Navbar = () => {
+  const { connect, disconnect, connected } = useWallet();
+  const [isHomePage, setIsHomePage] = useState(false);
+  const pathname = usePathname();
 
-    const [isClick, setisClick] = useState(false);
-    const [isHomePage, setIsHomePage] = useState(false);
-    const { connect }  = useWallet();
+  useEffect(() => {
+    setIsHomePage(pathname === "/home");
+  }, [pathname]);
 
-    
-    //check if current page is the home page
-    useEffect(() => {
-      if (window.location.pathname === "/home") {
-        setIsHomePage(true);
-      } else {
-        setIsHomePage(false);
-      }
-    }, []);
-    
+  return (
+    <nav className="bg-blue-600 fixed w-full z-20 top-0 start-0 border-b border-blue-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <Image src={logo} className="h-8" alt="VaultLock Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">VaultLock</span>
+                </Link>
+        
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          {isHomePage ? (
 
-    const dropDown = () => {
-        setisClick(!isClick);
-    };
-
-    const connectWallet = () => {
-      connect();
-    }
-
-    
-
-    return (
-        <nav id="nav" className="bg-blue-600  fixed w-full z-20 top-0 start-0 border-b border-blue-200">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <Image src={logo} className="h-8" alt="VaultLock Logo"/>
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">VaultLock</span>
-        </Link>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            {isHomePage ? (
-               <div className="relative">
-               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                   <svg className="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http:www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                   </svg>
-               </div>
-               <input type="search" id="default-search" className="block w-full lg:w-96 p-4 ps-10 text-sm text-black rounded-full shadow bg-gray-100" placeholder="Search Passwords, Urls..." required />
-               <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2">Search</button>
-           </div>
-            ): (
-          <button 
-          type="button" 
-          onClick={connectWallet}
-          className="text-black bg-white hover:bg-sky-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center ">
-              Connect Wallet
+            <div className="relative">
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full lg:w-96 p-4 ps-10 text-sm text-black rounded-full shadow bg-gray-100"
+                placeholder="Search Passwords, Urls..."
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 font-medium rounded-full text-sm px-4 py-2"
+              >
+                Search
               </button>
-            )}
-          <button
-          onClick={dropDown}
-          data-collapse-toggle="navbar-sticky"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-sky-100"
-          aria-controls="navbar-sticky"
-          aria-expanded={isClick}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-      </div>
-              
-        <div 
-          className={`${
-            isClick ? "block" : "hidden"
-          } items-center justify-between w-full md:flex md:w-auto md:order-1`}
-          id="navbar-sticky"
-        >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-            <li>  
-                <ScrollLink to="hero" smooth={true} duration={500} className="block py-2 px-3 text-white rounded hover:text-blue-100 md:bg-transparent md:text-white md:p-0 md:hover:text-blue-100 hover:underline" aria-current="page">Home</ScrollLink>
-            </li>
-            <li>
-                <ScrollLink to="services" smooth={true} duration={500} className="block py-2 px-3 text-white rounded hover:text-blue-100 md:hover:bg-transparent md:hover:text-blue-100 md:p-0">Services</ScrollLink>
-            </li>
-            <li>
-                <ScrollLink to="how-to" smooth={true} duration={500} className="block py-2 px-3 text-white rounded hover:text-blue-100 md:hover:bg-transparent md:hover:text-blue-100 md:p-0">How To</ScrollLink>
-            </li>
-            </ul>
-        </div>
-        </div>
-        </nav>
-    );
+            </div>
+          ) : (
 
+            connected ? (
+              <button
+                onClick={disconnect}
+                className="text-black bg-white hover:bg-sky-100 font-medium rounded-full text-sm px-4 py-2"
+              >
+                Disconnect Wallet
+              </button>
+            ) : (
+
+              <button
+                onClick={connect}
+                className="text-black bg-white hover:bg-sky-100 font-medium rounded-full text-sm px-4 py-2"
+              >
+                Connect Wallet
+              </button>
+            )
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- client/components/Wallet.tsx
+++ client/components/Wallet.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React, { createContext, useState, useContext } from 'react';
 import Web3 from 'web3';
-import abi from '../../server/contract/vault.json'; 
+import abi from '../../server/contract/vault.json';
 import { useRouter } from 'next/navigation';
 
 interface WalletContextType {
   connect: () => Promise<void>;
+  disconnect: () => void;
+  connected: boolean;
   web3: Web3 | null;
   accounts: string[];
   contract: any;
@@ -14,16 +16,14 @@ interface WalletContextType {
 const WalletContext = createContext<WalletContextType | undefined>(undefined);
 
 export function contractInstance(web3: Web3) {
-  return new web3.eth.Contract(
-    abi as any,
-    "0x38cB7800C3Fddb8dda074C1c650A155154924C73"
-  );
+  return new web3.eth.Contract(abi as any, "0x38cB7800C3Fddb8dda074C1c650A155154924C73");
 }
 
 export const WalletProvider = ({ children }: { children: React.ReactNode }) => {
   const [web3, setWeb3] = useState<Web3 | null>(null);
   const [accounts, setAccounts] = useState<string[]>([]);
   const [contract, setContract] = useState<any>(null);
+  const [connected, setConnected] = useState(false);
 
   const router = useRouter();
 
@@ -34,6 +34,8 @@ export const WalletProvider = ({ children }: { children: React.ReactNode }) => {
         setWeb3(web3Instance);
         const accounts = await web3Instance.eth.requestAccounts();
         setAccounts(accounts);
+        setConnected(true);
+
         if (accounts.length > 0) {
           router.push('/home');
         }
@@ -48,8 +50,16 @@ export const WalletProvider = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
+  const disconnect = () => {
+    setWeb3(null);
+    setAccounts([]);
+    setContract(null);
+    setConnected(false);
+    router.push('/');
+  };
+
   return (
-    <WalletContext.Provider value={{ connect, web3, accounts, contract }}>
+    <WalletContext.Provider value={{ connect, disconnect, connected, web3, accounts, contract }}>
       {children}
     </WalletContext.Provider>
   );
